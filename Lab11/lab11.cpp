b7a8e480f5468e10c5ce2076e521abcd1e8ab793
tharusha-pathirana <129775359+tharusha-pathirana@users.noreply.github.com>
2023-05-23 17:45:07 +0530

    Add files via upload

---
+++ Lab11/lab11.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <climits>
+
+#define V 6
+
+int find_min_key(int key[], bool mstSet[]) {
+    int min = INT_MAX, minIndex;
+
+    for (int v = 0; v < V; v++) {
+        if (!mstSet[v] && key[v] < min) {
+            min = key[v];
+            minIndex = v;
+        }
+    }
+
+    return minIndex;
+}
+
+void printMST(int parent[], int graph[V][V]) {
+    std::cout << "Edge \tWeight\n";
+    for (int i = 1; i < V; i++) {
+        std::cout << parent[i] << " - " << i << "\t" << graph[i][parent[i]] << "\n";
+    }
+}
+
+void primMST(int graph[V][V]) {
+    
+    int parent[V];
+    int key[V];
+    bool mstSet[V];
+
+    for (int i = 0; i < V; i++) {
+        key[i] = INT_MAX;
+        mstSet[i] = false;
+    }
+
+    key[0] = 0;
+    parent[0] = -1;
+
+    for (int count = 0; count < V - 1; count++) {
+        int u = find_min_key(key, mstSet);
+
+        mstSet[u] = true;
+
+        for (int v = 0; v < V; v++) {
+            if (graph[u][v] && !mstSet[v] && graph[u][v] < key[v]) {
+                parent[v] = u;
+                key[v] = graph[u][v];
+            }
+        }
+    }
+
+    printMST(parent, graph);
+}
+
+int main() {
+    int graph[V][V] = { { 0, 3, 0, 0, 0, 1 },
+                        { 3, 0, 2, 1, 10, 0 },
+                        { 0, 2, 0, 3, 0, 5 },
+                        { 0, 1, 3, 0, 0, 0 },
+                        { 0, 10, 0, 5, 0, 4 },
+                        { 1, 0, 5, 0, 4, 0 } };
+
+    primMST(graph);
+
+    return 0;
+}
